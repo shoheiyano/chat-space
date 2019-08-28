d3336c6fe8a490d52e4b84a2fcd24a53e581b14d
shoheiyano <yspersona@yahoo.co.jp>
2019-08-29 01:00:07 +0900

    チャットメイン更新

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   devise_for :users 
   resources :users
   resources :groups, only: [:new, :create, :edit, :update] do
-    resources :messages, only: [:ndex, :create]
+    resources :messages, only: [:index, :create]
   end
 end
