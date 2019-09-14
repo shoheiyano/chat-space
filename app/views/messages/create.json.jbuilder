7e689c4eae2d49d861e1f075e2d0bf6eb466fff6
shoheiyano <yspersona@yahoo.co.jp>
2019-09-14 21:03:02 +0900

    時間の形式変更

--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,5 +1,5 @@
 json.(@message, :content, :image)
-json.created_at @message.created_at.strftime("%Y/%m/%d %H:%M")
+json.created_at @message.created_at.to_s(:default)
 json.user_name @message.user.name
 json.image @message.image.url
 json.id @message.id
--- config/application.rb
+++ config/application.rb
@@ -9,11 +9,13 @@ Bundler.require(*Rails.groups)
 module ChatSpace
   class Application < Rails::Application
     config.generators do |g|
-      config.i18n.default_locale = :ja
       g.stylesheets false
       g.javascripts false
       g.helper false
       g.test_framework false
     end
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
   end
 end
