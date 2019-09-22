a72d91772f2dff7858fade1f264996cbf0978b53
shoheiyano <yspersona@yahoo.co.jp>
2019-09-22 15:55:32 +0900

    載せる前の修正

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -49,8 +49,9 @@ $(function() {
 
   });
   $(document).on('click',".user-search-add",function(){
-    var name = $(this).data("data-user-name");
-        var user_id = $(this).data("data-user-id");
+    var name = $(this).data("user-name");
+    console.log(name)
+        var user_id = $(this).data("user-id");
         $(this).parent().remove();
         appendMembers(name, user_id);
   })
